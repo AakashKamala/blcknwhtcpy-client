15977a97b0803c8293ccdeabdb44dcc600da5bf2
Aakash <aakashkumar2908@gmail.com>
2024-03-15 22:52:09 +0530

    test commit2

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import './ProductDetails.css';
 import { Link } from 'react-router-dom';
+import { baseURL } from '../../URLS.JS';
 
 function ProductDetails() {
   const { _id } = useParams();
